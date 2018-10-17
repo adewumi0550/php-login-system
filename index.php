d29d0daebb48445f1e55d640cb7be74cd2b3d698
Emerald <33107062+adewumi0550@users.noreply.github.com>
2018-10-17 11:42:03 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>
+Login System For Registration
+</title>
+<head>
+<body>
+<h1> Login Panel</h1>
+<form>
+<input type="text" name='username'><br />
+
+
+<input type="password" name='username'><br />
+
+<br />
+
+<input type="submit" name='login'><br />
+
+
+</form>
+
+
+</body>
+</html>
